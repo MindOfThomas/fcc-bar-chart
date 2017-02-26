5b61de87d119e57fac094257a0b14f0283da3202
Thomas Mays <MindOfThomas@users.noreply.github.com>
2017-02-26 18:49:51 -0500

    Separate draw functions

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,5 @@
 const info = require('./info.js');
+const draw = require('./draw.js');
 const calc = require('./calculations.js');
 info.chart.lines = calc.generateLineNums(5);
 
@@ -55,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
              .on('mouseout', tooltip.mouseoutHandler);
 
   // add background rect for aesthetics
-  drawBar(
+  draw.bar(
     g,                  // elementToAppendTo
     info.bar.width,     // width
     info.chart.height,  // height
@@ -65,7 +66,7 @@ document.addEventListener('DOMContentLoaded', function() {
   );
 
   // add a rect to represent the value
-  drawBar(
+  draw.bar(
     g,
     info.bar.width,
     calc.adjY,
@@ -76,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // add a foreground rect that will be transparent unless the user is hovering on it
   // this way the entire column will highlight on hover
-  drawBar(
+  draw.bar(
     g,
     info.bar.width,
     info.chart.height,
@@ -86,7 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
   );
 
   // add x-axis label
-  drawLabel(
+  draw.label(
     svg,                       // elementToAppendTo
     calc.chartAdjWidth() / 2,  // x
     calc.chartAdjHeight(),     // y
@@ -94,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function() {
   );
 
   // add y-axis label
-  drawLabel(
+  draw.label(
     svg,
     '-' + (info.chart.height / 2),  // x (but acting as y because this label is rotated -90deg)
     info.margins.bottom / 2,       // y (but acting as x because this label is rotated -90deg)
@@ -102,78 +103,5 @@ document.addEventListener('DOMContentLoaded', function() {
     'rotate(-90)'             // transform property
   );
 
-  drawLines(svg);
+  draw.lines(svg);
 });
-
-function drawBar(elementToAppendTo, width, height, className, x, y) {
-  elementToAppendTo.append('rect')
-                   .attr('width', width)
-                   .attr('height', height)
-                   .attr('class', className)
-                   .attr('x', x)
-                   .attr('y', y);
-}
-
-function drawLabel(elementToAppendTo, x, y, text, transform) {
-  let label = elementToAppendTo.append('text')
-                               .attr('class', 'label')
-                               .attr('text-anchor', 'middle')
-                               .attr('x', x)
-                               .attr('y', y)
-                               .text(text);
-  if (transform) {
-    label.attr('transform', transform);
-  }
-}
-
-function drawLines(elementToAppendTo) {
-  for (var i = 0; i < info.chart.lines.length; i++) {
-    let lineValue = info.chart.lines[i];
-    let y = calc.adjY(lineValue);
-
-    // if this line's y coord is at or above the chart's height then don't draw this line
-    if (y >= info.chart.height) continue;
-
-    drawLine(
-      elementToAppendTo,   // elementToAppendTo
-      info.margins.left * 0.8,  // x1 (don't draw the line all the way through the margin)
-      y,                   // y1
-      calc.chartAdjWidth(),   // x2
-      y                    // y2
-    );
-  }
-
-  // add x-axis line
-  let xaxisY = info.chart.height;
-  let xaxisX1 = info.margins.left;
-  let xaxisX2 = calc.chartAdjWidth();
-  drawLine(
-    elementToAppendTo,  // elementToAppendTo
-    xaxisX1,            // x1
-    xaxisY,             // y1
-    xaxisX2,            // x2
-    xaxisY,             // y2
-    'axis'
-  );
-
-  // add y-axis line
-  let yaxisX = info.margins.left; // don't draw the line all the way through the margin
-  drawLine(
-    elementToAppendTo,  // elementToAppendTo
-    yaxisX,             // x1
-    0,                  // y1
-    yaxisX,             // x2
-    info.chart.height,        // y2
-    'axis'
-  );
-}
-
-function drawLine(elementToAppendTo, x1, y1, x2, y2, className) {
-  className = className || '';
-  elementToAppendTo.append('line')
-                   .attr('x1', x1)
-                   .attr('y1', y1)
-                   .attr('x2', x2)
-                   .attr('y2', y2)
-                   .attr('class', className);
-}
---
+++ src/js/draw.js
@@ -0,0 +1,74 @@
+const info = require('./info.js');
+const calc = require('./calculations.js');
+
+module.exports = {
+  bar: function(elementToAppendTo, width, height, className, x, y) {
+    elementToAppendTo.append('rect')
+                     .attr('width', width)
+                     .attr('height', height)
+                     .attr('class', className)
+                     .attr('x', x)
+                     .attr('y', y);
+  },
+  label: function(elementToAppendTo, x, y, text, transform) {
+    let label = elementToAppendTo.append('text')
+                                 .attr('class', 'label')
+                                 .attr('text-anchor', 'middle')
+                                 .attr('x', x)
+                                 .attr('y', y)
+                                 .text(text);
+    if (transform) {
+      label.attr('transform', transform);
+    }
+  },
+  lines: function(elementToAppendTo) {
+    for (var i = 0; i < info.chart.lines.length; i++) {
+      let lineValue = info.chart.lines[i];
+      let y = calc.adjY(lineValue);
+
+      // if this line's y coord is at or above the chart's height then don't draw this line
+      if (y >= info.chart.height) continue;
+
+      this.line(
+        elementToAppendTo,        // elementToAppendTo
+        info.margins.left * 0.8,  // x1 (don't draw the line all the way through the margin)
+        y,                        // y1
+        calc.chartAdjWidth(),     // x2
+        y                         // y2
+      );
+    }
+
+    // add x-axis line
+    let xaxisY = info.chart.height;
+    let xaxisX1 = info.margins.left;
+    let xaxisX2 = calc.chartAdjWidth();
+    this.line(
+      elementToAppendTo,  // elementToAppendTo
+      xaxisX1,            // x1
+      xaxisY,             // y1
+      xaxisX2,            // x2
+      xaxisY,             // y2
+      'axis'
+    );
+
+    // add y-axis line
+    let yaxisX = info.margins.left; // don't draw the line all the way through the margin
+    this.line(
+      elementToAppendTo,  // elementToAppendTo
+      yaxisX,             // x1
+      0,                  // y1
+      yaxisX,             // x2
+      info.chart.height,        // y2
+      'axis'
+    );
+  },
+  line: function(elementToAppendTo, x1, y1, x2, y2, className) {
+    className = className || '';
+    elementToAppendTo.append('line')
+                     .attr('x1', x1)
+                     .attr('y1', y1)
+                     .attr('x2', x2)
+                     .attr('y2', y2)
+                     .attr('class', className);
+  }
+}
