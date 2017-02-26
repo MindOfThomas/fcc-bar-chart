073e67700c32213c53f773316bbd4d8d064ea542
Thomas Mays <MindOfThomas@users.noreply.github.com>
2017-02-26 17:02:57 -0500

    Increase bottom margin

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,6 @@
 const barWidth = 2;
 const chartHeight = 350;
-const margins = {bottom: 25, left: 50};
+const margins = {bottom: 50, left: 50};
 
 const d3 = require('d3');
 const numeral = require('numeral');
