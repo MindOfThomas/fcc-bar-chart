35cd9625bee3725aacb49276858f018f2998c483
Thomas Mays <MindOfThomas@users.noreply.github.com>
2017-02-25 23:32:24 -0500

    Add x- and y-axis labels

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 const barWidth = 3;
 const chartHeight = 350;
+const margins = {bottom: 25, left: 25};
 
 const d3 = require('d3');
 const tooltip = require('./tooltip.js');
@@ -16,14 +17,30 @@ document.addEventListener('DOMContentLoaded', function() {
   let svg = d3.select('#graph-container')
               .append('svg')
               .attr('id', 'graph')
-              .attr('width', values.length * barWidth)
-              .attr('height', chartHeight)
-              .selectAll('g')
-              .data(values)
-              .enter();
+              .attr('width', margins.left + (values.length * barWidth))
+              .attr('height', chartHeight + margins.bottom)
+
+  svg.append('text')
+     .attr('class', 'label')
+     .attr('text-anchor', 'end')
+     .attr('x', (barWidth * values.length) / 2)
+     .attr('y', chartHeight + margins.bottom)
+     .text('Time');
+
+  svg.append('text')
+     .attr('class', 'label')
+     .attr('text-anchor', 'start')
+     .attr('x', '-' + (chartHeight / 2)) // x and y are flipped because of the rotation
+     .attr('y', margins.bottom / 2) // this might not always put the label within the chart's bounds
+     .attr('transform', 'rotate(-90)')
+     .text('Value');
+
 
   // add a g to group our rects
-  let g = svg.append('g')
+  let g = svg.selectAll('g')
+             .data(values)
+             .enter()
+             .append('g')
              .attr('width', barWidth)
              .attr('height', chartHeight)
              .attr('x', (d, i) => i * barWidth)
@@ -37,14 +54,14 @@ document.addEventListener('DOMContentLoaded', function() {
    .attr('width', barWidth)
    .attr('height', chartHeight)
    .attr('class', 'bg')
-   .attr('x', (d, i) => i * barWidth)
+   .attr('x', (d, i) => margins.left + (i * barWidth))
 
   // add a rect to represent the value
   g.append('rect')
    .attr('class', 'bar')
    .attr('width', barWidth)
    .attr('height', (d) => (d / maxValue) * chartHeight)
-   .attr('x', (d, i) => i * barWidth)
+   .attr('x', (d, i) => margins.left + (i * barWidth))
    .attr('y', (d) => chartHeight - ((d / maxValue) * chartHeight))
 
   // add a foreground rect that will be transparent unless the user is hovering on it
@@ -53,5 +70,5 @@ document.addEventListener('DOMContentLoaded', function() {
    .attr('width', barWidth)
    .attr('height', chartHeight)
    .attr('class', 'fg')
-   .attr('x', (d, i) => i * barWidth)
+   .attr('x', (d, i) => margins.left + (i * barWidth))
 });
