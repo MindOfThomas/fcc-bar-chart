2213f5c6ac96e85ce7b7659a9cd15298d68487ce
Thomas Mays <MindOfThomas@users.noreply.github.com>
2017-02-25 21:15:44 -0500

    Add tooltip module

---
+++ src/js/tooltip.js
@@ -0,0 +1,34 @@
+const d3 = require('d3');
+
+module.exports = {
+  add: function() {
+    let div = document.createElement('div');
+
+    div.id = 'tooltip';
+    div.style.position = 'absolute';
+    div.style.zIndex = '10';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+  },
+  mouseoverHandler: function(d) {
+    let tooltip = document.getElementById('tooltip');
+
+    tooltip.textContent = d;
+    tooltip.style.visibility = 'visible';
+  },
+  mousemoveHandler: function() {
+    let tooltip = document.getElementById('tooltip');
+
+    let newTop = (event.pageY - 10) + 'px';
+    let newLeft = (event.pageX + 10) + 'px';
+
+    tooltip.style.top = newTop;
+    tooltip.style.left = newLeft;
+  },
+  mouseoutHandler: function() {
+    let tooltip = document.getElementById('tooltip');
+
+    tooltip.style.visibility = 'hidden';
+  }
+}
